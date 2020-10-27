695a3b187d7018adfa248159794723f0125d02b9
infernosquad <infzanoza@gmail.com>
2020-10-27 06:39:57 -0700

    Add frontend

---
+++ frontend/webpack.config.js
@@ -0,0 +1,43 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+    entry: "./src/index.js",
+    output: {
+        path: path.join(__dirname, "/public/dist"),
+        filename: "build.js"
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            filename: "../index.html",
+            template: "./src/index.html",
+            hash: true,
+        })
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader"
+                },
+            },
+            {
+                test: /\.css$/,
+                use: ["style-loader", "css-loader"]
+            },
+            {
+                test: /\.svg$/,
+                use: [
+                    {
+                        loader: 'svg-url-loader',
+                        options: {
+                            limit: 10000,
+                        },
+                    },
+                ],
+            },
+        ]
+    }
+};
